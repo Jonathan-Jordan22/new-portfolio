672c60e052f26693c936776e42ae793fbd74521f
Jonathan Jordan <jonathan.d.jordan22@gmail.com>
2025-09-24 17:01:49 -0600

    Updated Resume

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,34 @@
-import { useState, useEffect } from "react"
-import "./style.css"
-import BackgroundLines from "../BackgroundLines"
-import Button from "../Button"
-import MenuIcon from "../../assets/Icon/menu.svg"
-import ArrowUpRightIcon from "../../assets/Icon/arrow-up-right.svg"
-import headerVideo from "../../assets/headerVideo.webm"
-import TextWriting from "../TextWriting"
-import { motion, useAnimation } from "framer-motion"
-import { useInView } from "react-intersection-observer"
-import ScrambleText from "../ScrambleText"
-import Time from "../Time"
-import NavMenu from "../NavMenu"
+import { useState, useEffect } from "react";
+import "./style.css";
+import BackgroundLines from "../BackgroundLines";
+import Button from "../Button";
+import MenuIcon from "../../assets/Icon/menu.svg";
+import ArrowUpRightIcon from "../../assets/Icon/arrow-up-right.svg";
+import headerVideo from "../../assets/headerVideo.webm";
+import resumePDF from "../../assets/Images/Resume.docx.pdf";
+import TextWriting from "../TextWriting";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import ScrambleText from "../ScrambleText";
+import Time from "../Time";
+import NavMenu from "../NavMenu";
 
 export default function Header() {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-  const [hasAnimated, setHasAnimated] = useState(false)
-  const [menuVisible, setMenuVisible] = useState(false)
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
 
   const handleComplete = () => {
-    setHasAnimated(true)
-  }
+    setHasAnimated(true);
+  };
 
   useEffect(() => {
     // Start animation when the component is in view
     if (inView && !hasAnimated) {
-      controls.start("visible")
+      controls.start("visible");
     }
-  }, [inView, controls])
+  }, [inView, controls]);
 
   const nameVariants = {
     hidden: { scale: 0 },
@@ -40,22 +41,22 @@ export default function Header() {
         delay: 2.85,
       },
     },
-  }
+  };
 
   const opacityVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
-  }
+  };
 
   const blurVariants = {
     hidden: { opacity: 0, filter: "blur(10px)" },
     visible: { opacity: 1, filter: "blur(0px)" },
-  }
+  };
 
   const handleMenuToggle = () => {
-    console.log("toggle")
-    setMenuVisible((prev) => !prev)
-  }
+    console.log("toggle");
+    setMenuVisible((prev) => !prev);
+  };
 
   return (
     <header ref={ref}>
@@ -63,25 +64,45 @@ export default function Header() {
       <BackgroundLines />
       <NavMenu />
 
-      <motion.div initial="hidden" animate="visible" variants={blurVariants} transition={{ duration: 1, delay: 0.5 }} className="header--menuBtn">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={blurVariants}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="header--menuBtn"
+      >
         <Button icon={MenuIcon} onClick={handleMenuToggle} />
       </motion.div>
 
-      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 0.5 }} className="header--top">
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={blurVariants}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="header--top"
+      >
         <h3>
-          <a href="https://github.com/Jonathan-Jordan22" target={"_blank"} rel="noopener noreferrer">
+          <a
+            href="https://github.com/Jonathan-Jordan22"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
             <ScrambleText shuffle delay={0.5}>
               GITHUB
             </ScrambleText>
           </a>
           <span className="header--hash">{"//"}</span>
-          <a href="https://www.linkedin.com/in/jonathan-jordan-ab7719361" target={"_blank"} rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/jonathan-jordan-ab7719361"
+            target={"_blank"}
+            rel="noopener noreferrer"
+          >
             <ScrambleText shuffle delay={1}>
               LINKEDIN
             </ScrambleText>
           </a>
           <span className="header--hash">{"//"}</span>
-          <a href="/" target={"_blank"} rel="noopener noreferrer">
+          <a href={resumePDF} target={"_blank"} rel="noopener noreferrer">
             <ScrambleText shuffle delay={1.5}>
               RESUME
             </ScrambleText>
@@ -89,7 +110,13 @@ export default function Header() {
         </h3>
       </motion.div>
 
-      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 4 }} className="header--bottom">
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={blurVariants}
+        transition={{ duration: 1, delay: 4 }}
+        className="header--bottom"
+      >
         <div>
           <h3>
             <ScrambleText shuffle delay={4}>
@@ -99,7 +126,12 @@ export default function Header() {
           </h3>
           <p className="theme--detail">
             <ScrambleText shuffle delay={4}>
-              Hey, I’m Jonathan! 🚀 I love building sleek, modern web apps that feel effortless to use. Whether it’s designing smooth user experiences, solving tricky frontend challenges, or experimenting with new tools, I’m always pushing myself to learn and create better. My goal? To craft technology that makes people’s lives simpler, smarter, and more enjoyable.
+              Hey, I’m Jonathan! 🚀 I love building sleek, modern web apps that
+              feel effortless to use. Whether it’s designing smooth user
+              experiences, solving tricky frontend challenges, or experimenting
+              with new tools, I’m always pushing myself to learn and create
+              better. My goal? To craft technology that makes people’s lives
+              simpler, smarter, and more enjoyable.
             </ScrambleText>
           </p>
         </div>
@@ -109,13 +141,26 @@ export default function Header() {
         </h3>
       </motion.div>
 
-      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 4.5 }} className="header--center" onAnimationComplete={() => handleComplete()}>
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={blurVariants}
+        transition={{ duration: 1, delay: 4.5 }}
+        className="header--center"
+        onAnimationComplete={() => handleComplete()}
+      >
         <a href="#contact" className="connect--button">
           <Button label="Let’s connect" icon={ArrowUpRightIcon} />
         </a>
       </motion.div>
 
-      <motion.div initial="hidden" animate={controls} variants={blurVariants} transition={{ duration: 1, delay: 2.85 }} className="header--right">
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={blurVariants}
+        transition={{ duration: 1, delay: 2.85 }}
+        className="header--right"
+      >
         <h3>
           <span className="header--hash">{"//"}</span>{" "}
           <ScrambleText shuffle delay={2.9}>
@@ -125,16 +170,37 @@ export default function Header() {
         </h3>
       </motion.div>
 
-      <motion.div initial="hidden" animate={controls} variants={opacityVariants} transition={{ duration: 2, delay: 2.85 }} className="header--video">
+      <motion.div
+        initial="hidden"
+        animate={controls}
+        variants={opacityVariants}
+        transition={{ duration: 2, delay: 2.85 }}
+        className="header--video"
+      >
         <video src={headerVideo} autoPlay loop muted></video>
       </motion.div>
 
       <h1 className="header--name">
         <TextWriting controls={controls} text={"Jonathan Jordan"} noblink />
         <br />
-        <TextWriting controls={controls} delay={1.65} text={"Frontend"} noblink />{" "}
-        <motion.div initial="hidden" animate={controls} variants={nameVariants} className="header--name--sec">
-          <TextWriting controls={controls} delay={2.85} text={"Developer"} noblink />
+        <TextWriting
+          controls={controls}
+          delay={1.65}
+          text={"Frontend"}
+          noblink
+        />{" "}
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={nameVariants}
+          className="header--name--sec"
+        >
+          <TextWriting
+            controls={controls}
+            delay={2.85}
+            text={"Developer"}
+            noblink
+          />
           <div className="header--name--border">
             <span></span>
             <span></span>
@@ -144,5 +210,5 @@ export default function Header() {
         </motion.div>
       </h1>
     </header>
-  )
+  );
 }
